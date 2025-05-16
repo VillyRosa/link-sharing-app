c3ee34087235dedb3e3ea48f1ecc4f450bb17f8f
Villy Rosa <villyrosa69@gmail.com>
2025-05-16 13:31:06 -0300

    feat: created the rest of the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { LinksComponent } from './pages/links/links.component';
+import { ProfileDetailsComponent } from './pages/profile-details/profile-details.component';
+import { PreviewComponent } from './pages/preview/preview.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'links', component: LinksComponent },
+  { path: 'profile-details', component: ProfileDetailsComponent },
+  { path: 'preview', component: PreviewComponent },
 ];
