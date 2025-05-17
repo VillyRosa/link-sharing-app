ab26b87c1d0f2a0d324b9df72594f9d53c0aca6b
Villy Rosa <villyrosa69@gmail.com>
2025-05-16 23:37:37 -0300

    feat: build create account page

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { SvgInlineComponent } from "../../components/svg-inline/svg-inline.component";
+import { InputComponent } from "../../components/input/input.component";
+import { ButtonComponent } from "../../components/button/button.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
-  imports: [],
+  imports: [SvgInlineComponent, InputComponent, ButtonComponent, RouterModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
