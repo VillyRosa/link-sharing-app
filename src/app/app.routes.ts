f4bffed60da9c244b5325f06ca422c096dccd991
Villy Rosa <villyrosa69@gmail.com>
2025-05-16 23:27:28 -0300

    feat: create main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,18 @@ import { CreateAccountComponent } from './pages/create-account/create-account.co
 import { LinksComponent } from './pages/links/links.component';
 import { ProfileDetailsComponent } from './pages/profile-details/profile-details.component';
 import { PreviewComponent } from './pages/preview/preview.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'links', component: LinksComponent },
+      { path: 'profile-details', component: ProfileDetailsComponent },
+      { path: 'preview', component: PreviewComponent }
+    ]
+  },
   { path: 'login', component: LoginComponent },
   { path: 'create-account', component: CreateAccountComponent },
-  { path: 'links', component: LinksComponent },
-  { path: 'profile-details', component: ProfileDetailsComponent },
-  { path: 'preview', component: PreviewComponent },
 ];
