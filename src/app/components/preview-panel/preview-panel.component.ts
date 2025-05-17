6128f601af9139b2dec79033f2c34b400557baa0
Villy Rosa <villyrosa69@gmail.com>
2025-05-17 15:49:37 -0300

    feat: create preview component and add to layout

---
+++ src/app/components/preview-panel/preview-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-panel',
+  imports: [],
+  templateUrl: './preview-panel.component.html',
+  styleUrl: './preview-panel.component.scss'
+})
+export class PreviewPanelComponent {
+
+}
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
+import { PreviewPanelComponent } from "../../components/preview-panel/preview-panel.component";
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavComponent],
+  imports: [RouterOutlet, NavComponent, PreviewPanelComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
