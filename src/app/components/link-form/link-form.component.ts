f2ce505a5d019e3be10549e9a9618bd119dc6803
Villy Rosa <villyrosa69@gmail.com>
2025-05-19 22:19:53 -0300

    feat: create select component

--- src/app/components/link-form/link-form.component.ts
+++ src/app/components/link-form/link-form.component.ts
@@ -1,15 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { Link } from '../../models/Link';
 import { SvgInlineComponent } from "../svg-inline/svg-inline.component";
-import { ButtonComponent } from '../button/button.component';
 import { InputComponent } from '../input/input.component';
+import { SelectComponent } from "../select/select.component";
+import { SelectOption } from '../../models/SelectOption';
 
 @Component({
   selector: 'app-link-form',
-  imports: [SvgInlineComponent, ButtonComponent, InputComponent],
+  imports: [SvgInlineComponent, InputComponent, SelectComponent],
   templateUrl: './link-form.component.html',
   styleUrl: './link-form.component.scss'
 })
 export class LinkFormComponent {
+  public platformsOptions: SelectOption[] = [
+    { label: 'GitHub', value: 'github', icon: './images/svg/icon-github.svg' },
+    { label: 'GitLab', value: 'gitlab', icon: './images/svg/icon-gitlab.svg' },
+    { label: 'Code Pen', value: 'codepen', icon: './images/svg/icon-codepen.svg' },
+    { label: 'Code Wars', value: 'codewars', icon: './images/svg/icon-codewars.svg' },
+    { label: 'YouTube', value: 'youtube', icon: './images/svg/icon-youtube.svg' },
+    { label: 'Email', value: 'email', icon: './images/svg/icon-email.svg' },
+    { label: 'Frontend Mentor', value: 'frontend-mentor', icon: './images/svg/icon-frontend-mentor.svg' },
+    { label: 'Free Code Camp', value: 'freecodecamp', icon: './images/svg/icon-freecodecamp.svg' },
+    { label: 'Hashnode', value: 'hashnode', icon: './images/svg/icon-hashnode.svg' },
+    { label: 'Facebook', value: 'facebook', icon: './images/svg/icon-facebook.svg' },
+    { label: 'Linkedin', value: 'linkedin', icon: './images/svg/icon-linkedin.svg' },
+    { label: 'Twitch', value: 'twitch', icon: './images/svg/icon-twitch.svg' },
+    { label: 'Twitter', value: 'twitter', icon: './images/svg/icon-twitter.svg' },
+  ];
+
   @Input() link!: Link;
 }
---
+++ src/app/components/select/select.component.ts
@@ -0,0 +1,41 @@
+import { Component, ElementRef, EventEmitter, HostListener, inject, Input, Output } from '@angular/core';
+import { SelectOption } from '../../models/SelectOption';
+import { SvgInlineComponent } from "../svg-inline/svg-inline.component";
+
+@Component({
+  selector: 'app-select',
+  imports: [SvgInlineComponent],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss'
+})
+export class SelectComponent {
+  private elementRef = inject(ElementRef);
+  public isOpen: boolean = false;
+  
+  @Input() label: string = '';
+  @Input() name: string = '';
+  @Input() options: SelectOption[] = [];
+  @Input() selectedValue: string = '';
+  @Output() selectedValueChange = new EventEmitter<string>();
+
+  public toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  public selectOption(value: string) {
+    this.selectedValue = value;
+    this.selectedValueChange.emit(value);
+    this.isOpen = false;
+  }
+
+  public getSelectedOption(): SelectOption | undefined {
+    return this.options.find(option => option.value === this.selectedValue);
+  }
+
+  @HostListener('document:click', ['$event'])
+  public onClickOutside(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+  }
+}
