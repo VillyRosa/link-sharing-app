30166d6739519458501986728a09da3edf8583ed
Villy Rosa <villyrosa69@gmail.com>
2025-05-22 20:19:17 -0300

    chore: change app name to devlinks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'link-sharing-app' title`, () => {
+  it(`should have the 'devlinks' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('link-sharing-app');
+    expect(app.title).toEqual('devlinks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, link-sharing-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, devlinks');
   });
 });
